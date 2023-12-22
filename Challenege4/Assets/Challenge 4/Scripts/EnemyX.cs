7dac1b76a3c3ea41065884948708131d3fcdb449
Josipcuric <b00151073@mytudublin.ie>
2023-12-22 16:38:29 +0000

    add2

---
+++ Challenege4/Assets/Challenge 4/Scripts/EnemyX.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyX : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody enemyRb;
+    private GameObject playerGoal;
+    private SpawnManagerX spawnManagerXScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+
+        // Find the player goal object by its tag
+        playerGoal = GameObject.FindGameObjectWithTag("Player Goal");
+
+        // Corrected GetComponent call
+        spawnManagerXScript = GameObject.Find("Spawn Manager").GetComponent<SpawnManagerX>();
+        speed = spawnManagerXScript.enemySpeed;
+
+        // Check if the playerGoal is found
+        if (playerGoal == null)
+        {
+            Debug.LogError("Player Goal not found. Make sure it has the correct tag or is present in the scene.");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check if playerGoal is assigned
+        if (playerGoal != null)
+        {
+            // Set enemy direction towards player goal and move there
+            Vector3 lookDirection = (playerGoal.transform.position - transform.position).normalized;
+            enemyRb.AddForce(lookDirection * speed * Time.deltaTime);
+        }
+        else
+        {
+            Debug.LogWarning("Player Goal");
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        // If enemy collides with either goal, destroy it
+        if (other.gameObject.CompareTag("Enemy Goal") || other.gameObject.CompareTag("Player Goal"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
